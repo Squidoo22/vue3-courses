136f39164646920c413d9b6dbc4da6df0825945e
Dmytro Kolotilin <dmytro.kolotili@henryscheinone.com>
2023-02-13 18:00:38 +0200

    fix url

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    base: process.env.VITE_API_URL,
     define: {
         'process.env': {}
     },
